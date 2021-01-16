fafeaa9c47018da593c6ebc578bb0db2ba5d46fd
zenocomic <monkey-banana@windowslive.com>
2021-01-16 15:55:51 +0700

    exerice_2

--- String/string_exercise_2.py
+++ String/string_exercise_2.py
@@ -3,15 +3,15 @@ print(len(string))
 #จงเขียนคำสั่งเพื่อแสดงอักษรแรกของข้อความ "I am the best programmer"
 print(string[0])
 #จงเขียนคำสั่งเพื่อแสดง "best" ของข้อความ "I am the best programmer"
-print(string[9:12])
+print(string[9:13])
 #จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ที่ไม่มี space
-print(string.split(" "))
+print(string.replace(" ", ""))
 #จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ให้เป็นตัวพิมใหญ่ทั้งหมด
 print(string.upper())
 #จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ให้เป็นตัวพิมเล็กทั้งหมด
 print(string.lower())
 #จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ที่ถูกแทนที่อักษร e ด้วย z ทั้งหมด
-print(string.replace("e", "z")) #gg ez
+print(string.replace("e", "z"))
 #จงเติมคำในช่องว่าเพื่อแสดงชื่อ
 myname = "tatchai"
 txt = "{} is the best programmer"
