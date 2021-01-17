b39a40d5cc532f35cd3ad73971ece67583234427
zenocomic <monkey-banana@windowslive.com>
2021-01-17 14:12:15 +0700

    add list_exercise_2

---
+++ Python lists/list_exercise_2.py
@@ -0,0 +1,27 @@
+#จงแสดง "rat"
+animal = ["cat", "bat", "rat", "dog"]
+print(animal[2])
+
+#จงแก้ไขข้อมูลจาก "cat" เป็น "hen"
+animal = ["cat", "bat", "rat", "dog"]
+animal[0] = hen
+
+#จงเพิ่ม "hen" ไปยัง animal list
+animal = ["cat", "bat", "rat", "dog"]
+animal.append("orange")
+
+#จงเพิ่ม "hen" ไประหว่าง "rat" กับ "ิิdog"
+animal = ["cat", "bat", "rat", "dog"]
+animal.append(3,"orange")
+
+#จงลบ "rat" จาก list
+animal = ["cat", "bat", "rat", "dog"]
+del animal[2]
+
+#จงแสดงตัวสุดท้ายของ animal
+animal = ["cat", "bat", "rat", "dog"]
+print(animal[-1])
+
+#จงแสดงจำนวนของ animal
+animal = ["cat", "bat", "rat", "dog"]
+print(len(animal))
